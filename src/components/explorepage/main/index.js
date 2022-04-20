92d23dd53f92a982d77f7ea30982cd226499564f
Ankush Thakur <atpara99@gmail.com>
2022-04-20 13:00:40 +0530

    Text Changed

--- src/components/explorepage/main/index.js
+++ src/components/explorepage/main/index.js
@@ -1,45 +1,53 @@
 import { Link } from "react-router-dom";
-import explore from '../../../assets/explore.svg'
-import tattoo from '../../../assets/tattoo/tattoo.png'
+import explore from "../../../assets/explore.svg";
+import tattoo from "../../../assets/tattoo/tattoo.png";
 
-
-function Main(){
-    return(
-        <section className=" dark:bg-[#202124]  dark:text-white  min-h-screen lg:pl-60 lg:px-36 2xl:pt-28  lg:pt-16 ">
-          <div className=' justify-center lg:justify-between flex flex-wrap   '> 
-               <div className='  w-full lg:w-4/12 '>
-                   <div className='pt-10 lg:pt-20 font-bold text-2xl lg:text-6xl text-center lg:text-left 2xl:text-8xl  '>
-                       <h2>Explore</h2>
-                   </div>
-                   <p className='dark:text-[#B5B5B5] montserrat pt-4 lg:pt-6  text-[#343434] text-center lg:text-left 2xl:text-3xl 2xl:pt-16'>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed d</p>
-               </div>
-               <div className=' flex justify-center lg:justify-end  pt-10 lg:pt-0 w-4/6 lg:w-8/12'>
-                   <div className='w-full'>
-                       <img  className="w-full" src={explore}/>
-                   </div>
-               </div>
+function Main() {
+  return (
+    <section className=" dark:bg-[#202124]  dark:text-white  min-h-screen lg:pl-60 lg:px-36 2xl:pt-28  lg:pt-16 ">
+      <div className=" justify-center lg:justify-between flex flex-wrap   ">
+        <div className="  w-full lg:w-4/12 ">
+          <div className="pt-10 lg:pt-20 font-bold text-2xl lg:text-6xl text-center lg:text-left 2xl:text-8xl  ">
+            <h2>Explore</h2>
           </div>
-          <div className='flex pt-14 lg:pt-20 lg:pb-16  2xl:py-20 min-h-screen flex-wrap justify-between '>
-              <div className= 'flex justify-center w-full lg:w-6/12'>
-                   <div className=' w-full pt-4   '>
-                       <img className="w-full" src={tattoo}/>
-                   </div>
-              </div>
-              <div className=' w-full  lg:w-6/12 lg:pt-36 lg:pl-20  2xl:pt-36'>
-                  <div className=' pt-4 text-center lg:text-left text-2xl lg:text-5xl 2xl:text-7xl  montserrat font-semibold '>
-                      <h3>Tattoos</h3>
-                  </div>
-                  <p className='dark:text-[#B5B5B5]  pt-6 px-8 lg:px-0 text-center lg:text-xl lg:text-left 2xl:text-3xl 2xl:pt-10'>
-                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore  sed diam nonumy eirmod tempor invidunt ut labore 
-                  </p>
-                  <Link to="/Tattoos" >   
-                  <div className=' flex justify-center lg:justify-start lg:pt-16 2xl:pt-20  py-6'>
-                      <button className='dark:bg-[#8AB4F8] dark:text-black  button-shadow rounded-lg 2xl:rounded-2xl  bg-[#1A73E8] text-[#FFFFFF] px-10 py-1 text-sm montserrat lg:px-14 lg:py-2 lg:rounded-xl 2xl:px-20 2xl:py-3 2xl:text-2xl 2xl:font-bold'>Explore</button>
-                  </div>
-                  </Link>
-              </div>
+          <p className="dark:text-[#B5B5B5] montserrat pt-4 lg:pt-6  text-[#343434] text-center lg:text-left 2xl:text-3xl 2xl:pt-16">
+            DIMENSIONLESS Creative Ideas, get tattooed, buy art and join our
+            very dynamic group DNA of creative people.
+          </p>
+        </div>
+        <div className=" flex justify-center lg:justify-end  pt-10 lg:pt-0 w-4/6 lg:w-8/12">
+          <div className="w-full">
+            <img className="w-full" src={explore} />
           </div>
-        </section>
-    )
+        </div>
+      </div>
+      <div className="flex pt-14 lg:pt-20 lg:pb-16  2xl:py-20 min-h-screen flex-wrap justify-between ">
+        <div className="flex justify-center w-full lg:w-6/12">
+          <div className=" w-full pt-4   ">
+            <img className="w-full" src={tattoo} />
+          </div>
+        </div>
+        <div className=" w-full  lg:w-6/12 lg:pt-36 lg:pl-20  2xl:pt-36">
+          <div className=" pt-4 text-center lg:text-left text-2xl lg:text-5xl 2xl:text-7xl  montserrat font-semibold ">
+            <h3>Tattoos</h3>
+          </div>
+          <p className="dark:text-[#B5B5B5]  pt-6 px-8 lg:px-0 text-center lg:text-xl lg:text-left 2xl:text-3xl 2xl:pt-10">
+            We Do Custom tattoos based on your ideas style and comfort in three
+            simple steps.
+            <br /> <br />
+            Our tattoo studio is located in Bangalore. We are open on
+            appointment basis.
+          </p>
+          <Link to="/Tattoos">
+            <div className=" flex justify-center lg:justify-start lg:pt-16 2xl:pt-20  py-6">
+              <button className="dark:bg-[#8AB4F8] dark:text-black  button-shadow rounded-lg 2xl:rounded-2xl  bg-[#1A73E8] text-[#FFFFFF] px-10 py-1 text-sm montserrat lg:px-14 lg:py-2 lg:rounded-xl 2xl:px-20 2xl:py-3 2xl:text-2xl 2xl:font-bold">
+                Explore
+              </button>
+            </div>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 }
-export default Main
+export default Main;
--- src/components/explorepage/workshop/index.js
+++ src/components/explorepage/workshop/index.js
@@ -1,50 +1,56 @@
 import { Link } from "react-router-dom";
-import work from '../../../assets/workshops/workshops.png'
-import training from '../../../assets/Training-illustrations.svg'
-function Workshop(){
-    return(
-        <section className='darkmode bg-[#EEEEEE] p-6 lg:px-40'>
-        <div className= 'darkmode bg-[#EEEEEE] flex justify-evenly flex-wrap '>
-             <div className=' pt-10 lg:py-20 pb-14 w-full p-4 lg:w-6/12'>
-                  <div className='dark:text-white montserrat   font-semibold text-center lg:text-left text-2xl lg:text-5xl 2xl:text-left 2xl:text-7xl'>
-                      <h3>Workshops</h3>
-                  </div>
-                  <p className='pt-2 lg:pt-4  2xl:pt-8 text-center lg:text-left lg:text-xl  2xl:text-4xl'>
-                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy 
-                  </p>
-                  <div className='py-6 lg:pt-10 2xl:pt-12  text-center lg:text-left'>
-                    <Link to="/workshop">
-                     <button className='dark:bg-[#8AB4F8] dark:text-black button-shadow rounded-lg  lg:px-14 lg:py-2 lg:rounded-xl 2xl:rounded-2xl bg-[#1A73E8] text-[#FFFFFF] px-10 py-1 text-sm montserrat 2xl:px-20 2xl:py-3 2xl:text-2xl 2xl:font-bold'>Explore</button>
-                     </Link>
-                  </div>
-             </div>
-             <div className='darkmode flex items-center justify-center 2xl:justify-end  w-full  lg:w-6/12'>
-                 <div className='w-full p-8 lg:p-4'>
-                     <img  className="w-full" src={work}/>
-                 </div>
-             </div>
+import work from "../../../assets/workshops/workshops.png";
+import training from "../../../assets/Training-illustrations.svg";
+function Workshop() {
+  return (
+    <section className="darkmode bg-[#EEEEEE] p-6 lg:px-40">
+      <div className="darkmode bg-[#EEEEEE] flex justify-evenly flex-wrap ">
+        <div className=" pt-10 lg:py-20 pb-14 w-full p-4 lg:w-6/12">
+          <div className="dark:text-white montserrat   font-semibold text-center lg:text-left text-2xl lg:text-5xl 2xl:text-left 2xl:text-7xl">
+            <h3>Workshops</h3>
+          </div>
+          <p className="pt-2 lg:pt-4  2xl:pt-8 text-center lg:text-left lg:text-xl  2xl:text-4xl">
+            We conduct art based workshops which you can join and improve your
+            art, tattoos and visual skills
+          </p>
+          <div className="py-6 lg:pt-10 2xl:pt-12  text-center lg:text-left">
+            <Link to="/workshop">
+              <button className="dark:bg-[#8AB4F8] dark:text-black button-shadow rounded-lg  lg:px-14 lg:py-2 lg:rounded-xl 2xl:rounded-2xl bg-[#1A73E8] text-[#FFFFFF] px-10 py-1 text-sm montserrat 2xl:px-20 2xl:py-3 2xl:text-2xl 2xl:font-bold">
+                Explore
+              </button>
+            </Link>
+          </div>
         </div>
-        <div className='darkmode bg-[#EEEEEE] pt-8 lg:py-10 2xl:pt-40 flex justify-between flex-wrap '>
-           <div className=' flex items-center order-2 lg:order-1 justify-center w-full  lg:w-6/12'>
-               <div className='w-full p-8 lg:p-6'>
-                   <img className="w-full" src={training}/>
-               </div>
-           </div>
-           <div className=' lg:pl-16 darkmode w-full order-1  2xl:pl-16 p-6 lg:w-6/12'>
-                  <div className='dark:text-white montserrat  font-semibold text-center lg:text-left text-2xl lg:text-5xl 2xl:text-7xl'>
-                      <h3>Trainings</h3>
-                  </div>
-                  <p className='pt-2 lg:pt-4  2xl:pt-10 text-center lg:text-left lg:text-xl  2xl:text-4xl'>
-                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam 
-                  </p>
-                  <div className='py-8 lg:pt-10 2xl:pt-12 text-center lg:text-left'>
-                    <Link to="/training" >
-                     <button className='dark:bg-[#8AB4F8] dark:text-black button-shadow rounded-lg  lg:px-14 lg:py-2 lg:rounded-xl 2xl:rounded-2xl bg-[#1A73E8] text-[#FFFFFF] px-10  py-1 text-sm montserrat  2xl:px-20 2xl:py-3 2xl:text-2xl 2xl:font-bold'>Explore</button>
-                     </Link>
-                  </div>
-             </div>
-         </div>   
-  </section>
-    )
+        <div className="darkmode flex items-center justify-center 2xl:justify-end  w-full  lg:w-6/12">
+          <div className="w-full p-8 lg:p-4">
+            <img className="w-full" src={work} />
+          </div>
+        </div>
+      </div>
+      <div className="darkmode bg-[#EEEEEE] pt-8 lg:py-10 2xl:pt-40 flex justify-between flex-wrap ">
+        <div className=" flex items-center order-2 lg:order-1 justify-center w-full  lg:w-6/12">
+          <div className="w-full p-8 lg:p-6">
+            <img className="w-full" src={training} />
+          </div>
+        </div>
+        <div className=" lg:pl-16 darkmode w-full order-1  2xl:pl-16 p-6 lg:w-6/12">
+          <div className="dark:text-white montserrat  font-semibold text-center lg:text-left text-2xl lg:text-5xl 2xl:text-7xl">
+            <h3>Trainings</h3>
+          </div>
+          <p className="pt-2 lg:pt-4  2xl:pt-10 text-center lg:text-left lg:text-xl  2xl:text-4xl">
+            Also we conduct trainings for improving networking and business
+            skills to grow the art business as a whole.
+          </p>
+          <div className="py-8 lg:pt-10 2xl:pt-12 text-center lg:text-left">
+            <Link to="/training">
+              <button className="dark:bg-[#8AB4F8] dark:text-black button-shadow rounded-lg  lg:px-14 lg:py-2 lg:rounded-xl 2xl:rounded-2xl bg-[#1A73E8] text-[#FFFFFF] px-10  py-1 text-sm montserrat  2xl:px-20 2xl:py-3 2xl:text-2xl 2xl:font-bold">
+                Explore
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
-export default Workshop
+export default Workshop;
