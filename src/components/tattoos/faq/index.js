d3f5aa0aa66642c5071e49ee84e380b1d1c6fd65
Ankush Thakur <atpara99@gmail.com>
2022-06-07 18:28:00 +0530

    Text Added in FAQ section

--- src/components/tattoos/faq/index.js
+++ src/components/tattoos/faq/index.js
@@ -22,37 +22,64 @@ function Faq() {
         <Fragment>
           <Accordion open={open === 1} onClick={() => handleOpen(1)}>
             <AccordionHeader className="dark:text-black flex pl-4 lg:pl-0 lg:text-xl 2xl:text-4xl  montserrat font-semibold px-4">
-              What is Material Tailwind?
+              What is the Cost of getting a tattoo?
             </AccordionHeader>
             <AccordionBody className="text-sm text-center lg:text-lg lg:pt-4 2xl:text-2xl 2xl:pt-4 lg:text-left pt-2 px-12">
-              We&apos;re not always in the position that we want to be at.
-              We&apos;re constantly growing. We&apos;re constantly making
-              mistakes. We&apos;re constantly trying to express ourselves and
-              actualize our dreams.
+              Cost depends on time taken, design, complexity, placement etc. A
+              small tattoo starts from ₹3950. A medium size tattoo can cost
+              around ₹5500 - ₹9000.
+              <br /> <br /> A tattoo session of 4-6 hours for big tattoos is
+              usually ₹12000 - ₹20000. However we design the tattoo in your
+              budget too which you can flaunt for life. Consultation if free, we
+              understand your requirement and give our best try to design the
+              perfect tattoo for you.
             </AccordionBody>
           </Accordion>
 
           <Accordion open={open === 2} onClick={() => handleOpen(2)}>
             <AccordionHeader className="dark:text-black flex pl-4 lg:pl-0 lg:text-xl 2xl:text-4xl  montserrat font-semibold px-4">
-              How to use Material Tailwind?
+              Can you fix my tattoo or do a cover-up?
             </AccordionHeader>
             <AccordionBody className="text-sm text-center lg:text-lg lg:pt-4 2xl:text-2xl 2xl:pt-4 lg:text-left pt-2 px-12">
-              We&apos;re not always in the position that we want to be at.
-              We&apos;re constantly growing. We&apos;re constantly making
-              mistakes. We&apos;re constantly trying to express ourselves and
-              actualize our dreams.
+              We understand that sometime because of old decisions or getting a
+              tattoo from less known artist can make you carry a bad tattoo for
+              long and can be frustrating to live with it.
+              <br /> <br />
+              We have more than ten years of experience in coverups and
+              rectifying bad tattoos. We have team of artists who can give you
+              the best ideas in town. Also we can help in covering up scars
+              which you feel can be covered with a beautiful tattoo.
             </AccordionBody>
           </Accordion>
 
           <Accordion open={open === 3} onClick={() => handleOpen(3)}>
             <AccordionHeader className="dark:text-black flex pl-4 lg:pl-0 lg:text-xl 2xl:text-4xl  montserrat font-semibold px-4">
-              What can I do with Material Tailwind?
+              How painful is getting a tattoo?
             </AccordionHeader>
             <AccordionBody className="text-sm text-center lg:text-lg lg:pt-4 2xl:text-2xl 2xl:pt-4 lg:text-left pt-2 px-12">
-              We&apos;re not always in the position that we want to be at.
-              We&apos;re constantly growing. We&apos;re constantly making
-              mistakes. We&apos;re constantly trying to express ourselves and
-              actualize our dreams.
+              Tattooing is a very individualistic experience where different
+              clients experience pain differently. However there are certain
+              parts on the body which pains differently like knuckles , bones
+              and nerve areas.
+              <br /> <br />
+              We usually suggest to go natural and experience the tattooing in a
+              very cozy calm environment with nice music in the background so
+              that you remember the experience for lifetime. In addition we also
+              use numbing spray which can reduce the pain for small duration
+              tattoos (if client needs) .
+            </AccordionBody>
+          </Accordion>
+
+          <Accordion open={open === 4} onClick={() => handleOpen(4)}>
+            <AccordionHeader className="dark:text-black flex pl-4 lg:pl-0 lg:text-xl 2xl:text-4xl  montserrat font-semibold px-4">
+              What is the aftercare for tattoo?
+            </AccordionHeader>
+            <AccordionBody className="text-sm text-center lg:text-lg lg:pt-4 2xl:text-2xl 2xl:pt-4 lg:text-left pt-2 px-12">
+              After tattooing we give you a digital version or the aftercare /
+              in printout which you can follow for next 2 weeks.
+              <br />
+              Usually skin heals in 2-4 weeks depending on person. We guide you
+              throughout the process to ensure that you get the best end result.
             </AccordionBody>
           </Accordion>
         </Fragment>
